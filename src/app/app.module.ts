87abae666220637ee36b933edd750ca944ea884c
sabelogumede <iamsabelogumede@gmail.com>
2021-08-11 16:53:56 +0200

    added a card component to our app templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CardComponent } from './components/card/card.component';
 // import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    CardComponent
 
   ],
   imports: [
